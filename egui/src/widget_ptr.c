9e897c84c9e812bbc7047e92d97c16570207755a
sic49 <sic49@sic49.sicmarking.fr>
2013-02-08 12:03:12 +0100

    Forgot widget_ptr struct

---
+++ egui/src/widget_ptr.c
@@ -0,0 +1,35 @@
+#include <esic/egui/widget_ptr.h>
+
+static const vtable_Object s_vtable_object = {
+	WidgetPtr_destructor,
+	WidgetPtr_clone,
+	NULL,
+	NULL
+};
+
+PWidgetPtr WidgetPtr_constructor(PWidgetPtr self, PWidget widget) {
+
+	/* Filling members */
+	self->object.size = sizeof(WidgetPtr);
+	self->widget = widget;
+
+	/* Filling vtable */
+	self->object.vtable = &s_vtable_object;
+
+	return self;
+
+}
+
+void WidgetPtr_destructor(PObject self) {
+	/* Nothing do to */
+}
+
+PObject WidgetPtr_clone(PObject self, PObject dst) {
+	PWidgetPtr real_self = (PWidgetPtr)self;
+	PWidgetPtr real_dst  = (PWidgetPtr)dst;
+
+	/* Copying members */
+	dst->size        = self->size;
+	dst->vtable      = self->vtable;
+	real_dst->widget = real_self->widget;
+}
---
+++ includes/esic/egui/widget_ptr.h
@@ -0,0 +1,20 @@
+#ifndef _WIDGET_PTR_H_
+#define _WIDGET_PTR_H_
+
+#include <esic/egui/widget.h>
+
+
+/* In order to store pointers into containers such as Vector, List, and so on. */
+typedef struct _WidgetPtr {
+	Object object;
+	PWidget widget;
+} WidgetPtr, *PWidgetPtr;
+
+PWidgetPtr WidgetPtr_constructor(PWidgetPtr self, PWidget widget);
+
+/* Virtual functions */
+/* Object */
+void WidgetPtr_destructor(PObject self);
+PObject WidgetPtr_clone(PObject self, PObject dst);
+
+#endif /* _WIDGET_PTR_H_ */
