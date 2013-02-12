f76aa5f538c384407d624109fe7b78ba315522d0
sic49 <sic49@sic49.sicmarking.fr>
2013-02-12 16:31:10 +0100

    Forgotten lcd_painter

---
+++ includes/esic/elcd/lcd_painter.h
@@ -0,0 +1,23 @@
+#ifndef _LCD_PAINTER_H_
+#define _LCD_PAINTER_H_
+
+#include <esic/eapi/abstract_painter.h>
+
+typedef struct _LcdPainter {
+	AbstractPainter abstract_painter;
+} LcdPainter, *PLcdPainter;
+
+PLcdPainter LcdPainter_constructor(PLcdPainter self);
+
+/* Virtual functions */
+/* Object*/
+void LcdPainter_destructor(PObject self);
+
+/* AbstractPainter */
+void LcdPainter_drawLine(PAbstractPainter, WORD x1, WORD y1, WORD x2, WORD y2, WORD color);
+void LcdPainter_drawRectangle(PAbstractPainter, WORD x1, WORD y1, WORD x2, WORD y2, WORD background_color, WORD border_color);
+void LcdPainter_drawString(PAbstractPainter, WORD, WORD, WORD, const char*);
+void LcdPainter_drawPixel(PAbstractPainter, WORD, WORD, WORD);
+void LcdPainter_drawBuffer(PAbstractPainter, WORD, WORD, WORD, WORD, WORD*);
+
+#endif /* _LCD_PAINTER_H_ */
