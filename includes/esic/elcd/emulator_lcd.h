a263bacb0fb253c4c4f596098a2bc09610814fcd
sic49 <sic49@sic49.sicmarking.fr>
2013-04-19 10:12:09 +0200

    fixes in names for Lcd Emulation

--- includes/esic/elcd/emulator_lcd.h
+++ includes/esic/elcd/emulator_lcd.h
@@ -1,12 +1,12 @@
-#ifndef _LCD_EMULATOR_H_
-#define _LCD_EMULATOR_H_
+#ifndef _EMULATOR_LCD_H_
+#define _EMULATOR_LCD_H_
 
 //#include <SDL/SDL.h>
 
-void LcdEmulatorInit(DWORD width, DWORD height, BYTE bpp, DWORD flags);
-void LcdEmulatorDestroy();
-void LcdEmulatorUpdate();
-void LcdEmulatorSetPixel(DWORD x, DWORD y, DWORD color);
-void LcdEmulatorDrawLine(DWORD x1, DWORD y1, DWORD x2, DWORD y2, DWORD color);
+void EmulatorLcdInit(DWORD width, DWORD height, BYTE bpp, DWORD flags);
+void EmulatorLcdDestroy();
+void EmulatorLcdUpdate();
+void EmulatorLcdSetPixel(DWORD x, DWORD y, DWORD color);
+void EmulatorLcdDrawLine(DWORD x1, DWORD y1, DWORD x2, DWORD y2, DWORD color);
 
 #endif /* _LCD_EMULATOR_H_ */
